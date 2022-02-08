ac61d774dc0c8935291be12006aec0913e1d72bb
Jonathas <jonathas.campoi@gmail.com>
2022-02-08 20:51:05 -0300

    adição de tela de cadastro, com imputs e botão criar

---
+++ app/src/main/java/com/example/desafio_android_klever/ui/cadastro/CadastroFragment.kt
@@ -0,0 +1,21 @@
+package com.example.desafio_android_klever.ui.cadastro
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.desafio_android_klever.R
+
+class CadastroFragment : Fragment() {
+
+    private lateinit var viewModel: CadastroViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.cadastro_fragment, container, false)
+    }
+
+}
---
+++ app/src/main/java/com/example/desafio_android_klever/ui/cadastro/CadastroViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.desafio_android_klever.ui.cadastro
+
+import androidx.lifecycle.ViewModel
+
+class CadastroViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
