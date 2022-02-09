db4689a878488acb2b14e3dbbf5151261fe1859e
Jonathas <jonathas.campoi@gmail.com>
2022-02-08 23:02:11 -0300

    adição de repository, classe do banco e classe de dados

---
+++ app/src/main/java/com/example/desafio_android_klever/repository/CadastroRepository.kt
@@ -0,0 +1,17 @@
+package com.example.desafio_android_klever.repository
+
+import androidx.lifecycle.LiveData
+import com.example.desafio_android_klever.data.db.entity.CadastroEntity
+
+interface CadastroRepository {
+
+    suspend fun insertCadastro(nome: String, email: String): Long
+
+    suspend fun updateCadastro(id: Long, nome: String, email: String)
+
+    suspend fun deleteCadastro(id: Long)
+
+    suspend fun deleteAllCadastro()
+
+    suspend fun getAllCadastro(): LiveData<List<CadastroEntity>>
+}
---
+++ app/src/main/java/com/example/desafio_android_klever/repository/DatabaseSource.kt
@@ -0,0 +1,37 @@
+package com.example.desafio_android_klever.repository
+
+import androidx.lifecycle.LiveData
+import com.example.desafio_android_klever.data.db.dao.CadastroDAO
+import com.example.desafio_android_klever.data.db.entity.CadastroEntity
+
+class DatabaseSource(private val cadastroDAO: CadastroDAO) : CadastroRepository {
+
+    override suspend fun insertCadastro(nome: String, email: String): Long {
+        val cadastro = CadastroEntity(
+            nome = nome,
+            email = email
+        )
+        return cadastroDAO.insert(cadastro)
+    }
+
+    override suspend fun updateCadastro(id: Long, nome: String, email: String) {
+        val cadastro = CadastroEntity(
+            id = id,
+            nome = nome,
+            email = email
+        )
+        cadastroDAO.update(cadastro)
+    }
+
+    override suspend fun deleteCadastro(id: Long) {
+        cadastroDAO.delete(id)
+    }
+
+    override suspend fun deleteAllCadastro() {
+        cadastroDAO.deleteAll()
+    }
+
+    override suspend fun getAllCadastro(): LiveData<List<CadastroEntity>> {
+        return cadastroDAO.getAll()
+    }
+}
