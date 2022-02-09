7e9ffdc6b293ada1a0cbf18858cd1264f7a1ba76
Jonathas <jonathas.campoi@gmail.com>
2022-02-08 21:40:20 -0300

    adição e configuração da toolbar

--- app/src/main/java/com/example/desafio_android_klever/MainActivity.kt
+++ app/src/main/java/com/example/desafio_android_klever/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.desafio_android_klever
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import androidx.appcompat.widget.Toolbar;
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val appToolbar = findViewById<Toolbar>(R.id.app_toolbar)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        appToolbar.setupWithNavController(navController, appBarConfiguration)
+
     }
 }
