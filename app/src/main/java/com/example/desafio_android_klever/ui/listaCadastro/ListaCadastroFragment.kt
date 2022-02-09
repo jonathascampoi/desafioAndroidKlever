4739cf844974a519a43f004414d12ec6b76c369d
Jonathas <jonathas.campoi@gmail.com>
2022-02-09 18:23:29 -0300

    adição da view de listagem dos itens cadastrados

---
+++ app/src/main/java/com/example/desafio_android_klever/ui/listaCadastro/ListaCadastroFragment.kt
@@ -0,0 +1,9 @@
+package com.example.desafio_android_klever.ui.listaCadastro
+
+import androidx.fragment.app.Fragment
+import com.example.desafio_android_klever.R
+
+class ListaCadastroFragment : Fragment(R.layout.lista_cadastro_fragment) {
+
+
+}
---
+++ app/src/main/java/com/example/desafio_android_klever/ui/listaCadastro/ListaCadastroViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.desafio_android_klever.ui.listaCadastro
+
+import androidx.lifecycle.ViewModel
+
+class ListaCadastroViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
